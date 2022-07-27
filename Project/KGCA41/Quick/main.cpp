13040ed59367c2d40d090e5a0cedac4190fefe2d
sinsin950313 <sinsin950313@gmail.com>
2022-07-27 21:55:43 +0900

    <Fix> : Quick Sort changed.
    Quick Sort code is being changed meaningfully.

--- Project/KGCA41/Quick/main.cpp
+++ Project/KGCA41/Quick/main.cpp
@@ -11,7 +11,7 @@ int main()
 {
 	cout << "Quick Sort" << endl;
 	QuickSort quick(LENGTH);
-	//srand(static_cast<unsigned int>(time(NULL)));
+	srand(static_cast<unsigned int>(time(NULL)));
 	for (int i = 0; i < LENGTH; ++i)
 	{
 		quick.Push(rand() % 101);

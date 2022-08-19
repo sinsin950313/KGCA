4f948b9c9a8596db84d46aaccd837942cd7cf8c9
sin <sinsin950313@gmail.com>
2022-08-19 18:30:57 +0900

    <Chore> : QuadTree to CollisionTree
    Extract important part of QuadTree to CollisionTree.
    
    Now, need to realize SpaceDivider.
    If I realize this, this system will be working right.
    But, how to divide an arbitrary volume?

---
+++ Project/KGCA41/CollisionSystem/CollisionTree.cpp
@@ -0,0 +1,9 @@
+#include "CollisionTree.h"
+
+std::vector<Volume<2>*> QuadTreeDivider::Divide(const Volume<2>& volume)
+{
+}
+
+std::vector<Volume<3>*> OctTreeDivider::Divide(const Volume<3>& volume)
+{
+}
--- Project/KGCA41/CollisionSystem/main.cpp
+++ Project/KGCA41/CollisionSystem/main.cpp
@@ -1,6 +1,6 @@
 #include "Rigidbody.h"
 #include <iostream>
-#include "QuadTree.h"
+#include "CollisionTree.h"
 
 int main()
 {
@@ -10,49 +10,49 @@ int main()
 	Vector2D velocity = r2d.GetVelocity();
 	std::cout << velocity.Get(0) << ", " << velocity.Get(1) << std::endl;
 
-	constexpr int dimension = 2;
-	const int width = 100;
-	const int height = 100;
-	const int maxDepth = 3;
-	QuadTree<dimension> qt(width, height, maxDepth);
-	std::vector<Object<dimension>*> objects;
-	Object<dimension>* player;
-	std::vector<Object<dimension>*> collidedObjects;
+	//constexpr int dimension = 2;
+	//const int width = 100;
+	//const int height = 100;
+	//const int maxDepth = 3;
+	//CollisionTree<dimension> qt(width, height, maxDepth);
+	//std::vector<Object<dimension>*> objects;
+	//Object<dimension>* player;
+	//std::vector<Object<dimension>*> collidedObjects;
 
-	int objectCount = 100;
-	for (int i = 0; i < objectCount; ++i)
-	{
-		float centerX = (rand() % width) - (width / 2);
-		float centerY = (rand() % height) - (height / 2);
-		float tmpWidth = rand() % 10 + 1.0f;
-		float tmpHeight = rand() % 10 + 1.0f;
-		Object<dimension>* object = new Object<dimension>(new Rectangle(centerX, centerY, tmpWidth, tmpHeight), Rigidbody2D());
-		objects.push_back(object);
-		qt.AddObject(object);
-	}
+	//int objectCount = 100;
+	//for (int i = 0; i < objectCount; ++i)
+	//{
+	//	float centerX = (rand() % width) - (width / 2);
+	//	float centerY = (rand() % height) - (height / 2);
+	//	float tmpWidth = rand() % 10 + 1.0f;
+	//	float tmpHeight = rand() % 10 + 1.0f;
+	//	Object<dimension>* object = new Object<dimension>(new Rectangle(centerX, centerY, tmpWidth, tmpHeight), Rigidbody2D());
+	//	objects.push_back(object);
+	//	qt.AddObject(object);
+	//}
 
-	player = new Object<dimension>(new Rectangle(0, 0, 10, 10), Rigidbody2D());
+	//player = new Object<dimension>(new Rectangle(0, 0, 10, 10), Rigidbody2D());
 
-	float deltaTime = 0.1f;
-	player->GetRigidBody()->Calculate(deltaTime);
-	Vector2D pos = player->GetVolume()->GetCenter() + (player->GetRigidBody()->GetVelocity() * deltaTime);
-	player->GetVolume()->Reposition(pos);
+	//float deltaTime = 0.1f;
+	//player->GetRigidBody()->Calculate(deltaTime);
+	//Vector2D pos = player->GetVolume()->GetCenter() + (player->GetRigidBody()->GetVelocity() * deltaTime);
+	//player->GetVolume()->Reposition(pos);
 
-	for (auto iter = objects.begin(); iter != objects.end(); ++iter)
-	{
-		(*iter)->GetRigidBody()->Calculate(deltaTime);
-		Vector2D pos = (*iter)->GetVolume()->GetCenter() + (*iter)->GetRigidBody()->GetVelocity() * deltaTime;
-		(*iter)->GetVolume()->Reposition(pos);
-	}
+	//for (auto iter = objects.begin(); iter != objects.end(); ++iter)
+	//{
+	//	(*iter)->GetRigidBody()->Calculate(deltaTime);
+	//	Vector2D pos = (*iter)->GetVolume()->GetCenter() + (*iter)->GetRigidBody()->GetVelocity() * deltaTime;
+	//	(*iter)->GetVolume()->Reposition(pos);
+	//}
 
-	collidedObjects = qt.GetCollidedObjects(player);
+	//collidedObjects = qt.GetCollidedObjects(player);
 
-	const Volume<dimension>* playerVolume = player->GetVolume();
-	playerVolume->Print();
-	
-	for (auto iter = collidedObjects.begin(); iter != collidedObjects.end(); ++iter)
-	{
-		const Volume<dimension>* tmp = (*iter)->GetVolume();
-		tmp->Print();
-	}
+	//const Volume<dimension>* playerVolume = player->GetVolume();
+	//playerVolume->Print();
+	//
+	//for (auto iter = collidedObjects.begin(); iter != collidedObjects.end(); ++iter)
+	//{
+	//	const Volume<dimension>* tmp = (*iter)->GetVolume();
+	//	tmp->Print();
+	//}
 }

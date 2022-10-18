35bbd930635dd76d2f37f1df85b6436287e16882
sin <sinsin950313@gmail.com>
2022-10-18 19:03:01 +0900

    <Chore> : Constant Buffer and Map split.
    Just Constant buffer for shader is written.
    
    Model code file has Map.
    Map just implemented only split tiles.
    
    Structure initializer like Float3, Float4 need more than c++17.
    At c++17, codecvt is deprecated.
    So added "#pragma warning (disable : 4996)"

---
+++ Project/include/Model.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+#include "Vector.h"
+
+namespace SSB
+{
+	struct Vertex
+	{
+		Float4 position;
+		Float4 color;
+		Float2 texture;
+	};
+
+	class Terrain
+	{
+	private:
+		float _cellDistance = 1.0f;
+		float tileX = 10.0f;
+		float tileY = 10.0f;
+		std::vector<Vertex> _vertexList;
+		std::vector<int> _indexList;
+
+	public:
+		void Make(unsigned int widthVertexCount, unsigned int heightVertexCount);
+	};
+}

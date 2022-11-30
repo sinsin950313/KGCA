3638f882c8852467821532bc4dd4dcf0fe48f83c
sin <sinsin950313@gmail.com>
2022-11-30 12:38:59 +0900

    <Chore> : Unreal Git Commit Setting
    For now, able to commit Unreal code and update Unreal resources.

--- Load.sh
+++
@@ -1,1 +0,0 @@
-cp -r Project/Naver\ MYBOX/Resource/ Project/
--- Save.sh
+++ Save.sh
@@ -1,1 +1,7 @@
-cp -r Project/Resource/ Project/Naver\ MYBOX/
+echo "n" | cp -r -i Project/Resource/ Naver\ MYBOX/EngineData/
+
+for dir in Unreal/*/;
+do
+name="$(basename $dir)"
+echo "n" | cp -r -i Unreal/$name/Content/ Naver\ MYBOX/UnrealData/$name
+done

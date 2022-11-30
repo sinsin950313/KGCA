d00383329a4564b4060e5003546d0cb52f49cf67
sin <sinsin950313@gmail.com>
2022-11-30 16:38:00 +0900

    <Chore> : Save Resource except Blueprint
    Don't forget to positioning Blueprint at Blueprint directory.

--- Save.sh
+++ Save.sh
@@ -1,7 +1,20 @@
-echo "n" | cp -r -i Project/Resource/ Naver\ MYBOX/EngineData/
+#echo "n" | cp -r -i Project/Resource/ Naver\ MYBOX/EngineData/
 
-for dir in Unreal/*/;
+for Directory in Unreal/*/;
 do
-name="$(basename $dir)"
-echo "n" | cp -r -i Unreal/$name/Content/ Naver\ MYBOX/UnrealData/$name
+	ProjectName="$(basename $Directory)"
+
+	for SubDirectory in Unreal/$ProjectName/Content/*;
+	do
+		NewName="$(basename $SubDirectory)"
+		if [ -f $SubDirectory ];
+		then
+			mkdir -p Naver\ MYBOX/UnrealData/$ProjectName/Content/
+			echo "n" | cp -r -i Unreal/$ProjectName/Content/$NewName Naver\ MYBOX/UnrealData/$ProjectName/Content/
+		elif [ "$NewName" != "Blueprint" ];
+		then
+			mkdir -p Naver\ MYBOX/UnrealData/$ProjectName/Content/$NewName
+			echo "n" | cp -r -i Unreal/$ProjectName/Content/$NewName Naver\ MYBOX/UnrealData/$ProjectName/Content/$NewName
+		fi
+	done
 done

a52e69a417a4613898bda4e168d1fd4bf29236f9
sin <sinsin950313@gmail.com>
2022-12-13 11:26:54 +0900

    <Feat> : Save and Load rewriting

---
+++ Load.sh
@@ -0,0 +1,23 @@
+echo "n" | cp -r -i -v Naver\ MYBOX/EngineData/Resource Project
+
+for Directory in Naver\ MYBOX/UnrealData/*;
+do
+	ProjectName="$(basename "$Directory")"
+	mkdir -p Unreal/$ProjectName/Content/
+
+	for SubDirectory in Naver\ MYBOX/UnrealData/$ProjectName/Content/*
+	do
+		NewName="$(basename "$SubDirectory")"
+#		if [ -d "$SubDirectory" ];
+#		then
+#			mkdir -p Unreal/$ProjectName/Content/$NewName
+#		fi
+
+		if [ "$NewName" == "Map" ];
+		then
+			cp -r -f -v Naver\ MYBOX/UnrealData/$ProjectName/Content/$NewName Unreal/$ProjectName/Content/
+		else
+			echo "n" | cp -r -i -v Naver\ MYBOX/UnrealData/$ProjectName/Content/$NewName Unreal/$ProjectName/Content/
+		fi
+	done
+done
--- Save.sh
+++ Save.sh
@@ -1,25 +1,26 @@
+mkdir -p Naver\ MYBOX/EngineData/
 echo "n" | cp -r -i -v Project/Resource/ Naver\ MYBOX/EngineData/
 
 for Directory in Unreal/*/;
 do
 	ProjectName="$(basename $Directory)"
+	mkdir -p Naver\ MYBOX/UnrealData/$ProjectName/Content
 
 	for SubDirectory in Unreal/$ProjectName/Content/*;
 	do
 		NewName="$(basename $SubDirectory)"
 		if [ "$NewName" != "Blueprint" ];
 		then
-
-			if [ -d $SubDirectory ];
-			then
-				mkdir -p Naver\ MYBOX/UnrealData/$ProjectName/Content/$NewName
-			fi
+#			if [ -d $SubDirectory ];
+#			then
+#				mkdir -p Naver\ MYBOX/UnrealData/$ProjectName/Content/$NewName
+#			fi
 
 			if [ "$NewName" == "Map" ];
 			then
-				cp -r -f -v Unreal/$ProjectName/Content/$NewName Naver\ MYBOX/UnrealData/$ProjectName/Content
+				cp -r -f -v Unreal/$ProjectName/Content/$NewName Naver\ MYBOX/UnrealData/$ProjectName/Content/
 			else
-				echo "n" | cp -r -i -v Unreal/$ProjectName/Content/$NewName Naver\ MYBOX/UnrealData/$ProjectName/Content
+				echo "n" | cp -r -i -v Unreal/$ProjectName/Content/$NewName Naver\ MYBOX/UnrealData/$ProjectName/Content/
 			fi
 		fi
 	done

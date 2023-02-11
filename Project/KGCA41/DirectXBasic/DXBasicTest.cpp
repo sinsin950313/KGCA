ab7d43bf08108e68931f746a8f712243c6608212
sinsin950313 <sinsin950313@gmail.com>
2023-02-11 16:14:58 +0900

    <Chore> : Map and Camera prototyping

--- Project/KGCA41/DirectXBasic/DXBasicTest.cpp
+++ Project/KGCA41/DirectXBasic/DXBasicTest.cpp
@@ -44,17 +44,18 @@ namespace SSB
             _objectBack->Move({ 0, 0, 5 });
         }
 
-        {
-            _terrain = new Map();
-            SpriteLoader::GetInstance().RegisterSpriteWithCoordinateValue(L"KGCABK.bmp", L"Background", { 0, 0, 1024, 768 });
-            _terrain->SetSprite(SpriteLoader::GetInstance().Load(L"KGCABK.bmp", L"Background", DXStateManager::kDefaultWrapSample));
-            _terrain->SetVertexShader(ShaderManager::GetInstance().LoadVertexShader(L"Default3DVertexShader.hlsl", "Main", "vs_5_0"));
-            _terrain->SetPixelShader(ShaderManager::GetInstance().LoadPixelShader(L"Default3DPixelShader.hlsl", "Main", "ps_5_0"));
-            //_terrain->SetHeightMap(L"heightMap513.bmp");
-            _terrain->SetSize(3, 3);
-            _terrain->Init();
-            _terrain->SetPosition({ 0, 0, 0 });
-        }
+        //{
+        //    _terrain = new Map();
+        //    SpriteLoader::GetInstance().RegisterSpriteWithCoordinateValue(L"KGCABK.bmp", L"Background", { 0, 0, 1024, 768 });
+        //    _terrain->SetSprite(SpriteLoader::GetInstance().Load(L"KGCABK.bmp", L"Background", DXStateManager::kDefaultWrapSample));
+        //    _terrain->SetVertexShader(ShaderManager::GetInstance().LoadVertexShader(L"Default3DVertexShader.hlsl", "Main", "vs_5_0"));
+        //    _terrain->SetPixelShader(ShaderManager::GetInstance().LoadPixelShader(L"Default3DPixelShader.hlsl", "Main", "ps_5_0"));
+        //    //_terrain->SetHeightMap(L"heightMap513.bmp");
+        //    _terrain->SetSize(257, 257);
+        //    _terrain->SetMaxLayerLevel(1);
+        //    _terrain->Init();
+        //    _terrain->SetPosition({ 0, 0, 0 });
+        //}
 
         _dCamera = new DebugCamera();
         _dCamera->Init();
@@ -65,7 +66,7 @@ namespace SSB
         ChangeMainCamera(_dCamera);
         
         HVector4 pos = _object->GetPosition();
-        _object->SetPosition({ pos.GetX(), _terrain->GetHeight(pos.GetX(), pos.GetZ()), pos.GetZ() });
+        //_object->SetPosition({ pos.GetX(), _terrain->GetHeight(pos.GetX(), pos.GetZ()), pos.GetZ() });
         GetMainCamera()->Move(_object->GetPosition());
 
         return false;
@@ -86,7 +87,7 @@ namespace SSB
 		OutputDebugString((L"Time : " + std::to_wstring(_timer->GetElapseTime() / 1000.0f) + L" FPS : " + std::to_wstring(_fps) + L"\n").c_str());
 		}
         _object->Frame();
-        _terrain->Frame();
+        //_terrain->Frame();
         //_objectBack->Rotate(0.001f, 0, 0);
         _objectBack->Frame();
 
@@ -107,12 +108,12 @@ namespace SSB
             delete _objectBack;
             _objectBack = nullptr;
         }
-        if (_terrain)
-        {
-            _terrain->Release();
-            delete _terrain;
-            _terrain = nullptr;
-        }
+        //if (_terrain)
+        //{
+        //    _terrain->Release();
+        //    delete _terrain;
+        //    _terrain = nullptr;
+        //}
         if(_dCamera)
         {
             _dCamera->Release();
@@ -141,7 +142,7 @@ namespace SSB
     {
         DXWindow::PreRender();
         _object->Render();
-        _terrain->Render();
+        //_terrain->Render();
         _objectBack->Render();
         return true;
     }
--- Project/KGCA41/DirectXBasic/Map.h
+++ Project/KGCA41/DirectXBasic/Map.h
@@ -9,132 +9,89 @@
 #include "Camera.h"
 #include "CollisionDetector.h"
 #include "Box1.h"
+#include "CollisionDetector.h"
 
 namespace SSB
 {
-	class Map : public Common
+	class Map : public DXObject
 	{
 	private:
 		class MapModel : public Model
 		{
-			float _widthVertexCount;
-			float _heightVertexCount;
-			Map* _map;
+		private:
+			class MapModelVolume : public Box1Volume
+			{
+			private:
+				std::vector<MapModelVolume*> _childs;
+			};
+
+		private:
+			MapModelVolume* _root;
+			std::vector<MapModelVolume*> _renderVolumeList;
+
+		private:
+			float _celldistance = 1.0f;
+			float _heightScale = 100;
+			float tileX = 1.0f;
+			float tileY = 1.0f;
+			unsigned int _widthVertexCount = 256 + 1u;
+			unsigned int _heightVertexCount = 256 + 1u;
 
 		public:
-			MapModel(Map* map);
+			MapModel();
 
 		private:
 			Vector3 CalculateFaceNormal(UINT i0, UINT i1, UINT i2);
 			void CalculateVertexNormal();
-
-		public:
+			float GetCoordinateHeight(int x, int z);
 			float Lerp(float start, float end, float param);
 
 		private:
 			void Build() override;
 
 		public:
-			void Set(float widthVertexCount, float heightVertexCount);
+			void SetSize(float widthVertexCount, float heightVertexCount);
 			void SetHeightMap(std::wstring fileName);
-			float GetCoordinateHeight(int x, int z);
+			float GetHeight(int x, int z);
 
 		public:
-			std::vector<Vertex_PNCT> GetVertice();
+			// Render space division works here
+			bool Render() override;
+		};
+		class MapVolume : public Box1Volume
+		{
+		public:
+			std::vector<TriangleData> GetWorldBaseTriangles() override;
+		};
+		class RayToMapVolumeCollisionDetector : public CollisionDetectorInterface
+		{
+		public:
+			bool IsCollide(CollisionSystemVolume* ray, CollisionSystemVolume* mapVolume) override;
+			bool IsIn(CollisionSystemVolume* ray, CollisionSystemVolume* mapVolume) override;
+			std::vector<Vector3> GetIntersections(CollisionSystemVolume* ray, CollisionSystemVolume* mapVolume) override;
 		};
 
 	private:
-		class Node : public Common
-		{
-		private:
-			class NodeVolume : public Box1Volume
-			{
-			public:
-				std::vector<TriangleData> GetWorldBaseTriangles() override;
-			};
-
-		private:
-			std::vector<Node*> _nodes;
-			ID3D11Buffer* _indexBuffer;
-			std::vector<int> _indexList;
-
-			Map& _map;
-			int _leftTop;
-			int _leftBottom;
-			int _rightTop;
-			int _rightBottom;
-			int _center;
-			int _layerDepth;
-
-			float _width;
-			float _height;
-			float _depth;
-
-			HMatrix44 _matrix;
-			DXObject* _debugBox;
-
-		public:
-			Node(Map& map, int leftTop, int rightTop, int leftBottom, int rightBottom, int depth);
-
-		public:
-			bool CreateIndexBuffer();
-			void Check(std::vector<Node*>& drawingNodeList, Camera* camera);
-			ID3D11Buffer* GetIndexBuffer();
-			std::vector<int> GetIndexList();
-
-		public:
-			bool Init() override;
-			bool Frame() override;
-			bool Render() override;
-			bool Release() override;
-		};
-
-		MapModel _model;
-		ID3D11Buffer* _constantBuffer;
-		ConstantData _constantData;
-
-		HMatrix44 _matrix;
-
-		float _cellDistance = 1.0f;
-		float _heightScale = 100;
-		float tileX = 1.0f;
-		float tileY = 1.0f;
-		unsigned int _widthVertexCount = 256 + 1u;
-		unsigned int _heightVertexCount = 256 + 1u;
-
-		Node* _root;
-		int _layerDepth = 3;
-		std::vector<Node*> _drawingNodeList;
-
+		MapModel* _model;
 		ID3D11Texture2D* _heightTexture;
-
-		std::vector<float> _heightData;
-		float _height = 0;
+		std::vector<MapVolume*> _volumes;
 
 		bool _bDebug = false;
 
 	public:
-		Map(int layerDepth = 4);
-		virtual ~Map();
-
-	private:
-		bool CreateConstantBuffer();
-		void UpdateConstantBuffer();
-		float GetCoordinateHeight(int x, int z);
+		Map();
+		~Map() override;
 
 	public:
-		void SetSize(unsigned int widthVertexCount, unsigned int heightVertexCount);
-		void SetPosition(Vector3 vec);
-		void SetSprite(Sprite* sprite);
+		void SetComponentCount(unsigned int widthCount, unsigned int heightCount);
+		void SetSectionCount(unsigned int widthCount, unsigned int heightCount);
+		void SetSectionSize(unsigned int size);
 		void SetHeightMap(std::wstring fileName);
-		void SetVertexShader(Shader* shader);
-		void SetPixelShader(Shader* shader);
 		float GetHeight(float x, float z);
 
 	public:
 		bool Init() override;
 		bool Frame() override;
-		bool Render() override;
 		bool Release() override;
 	};
 }
